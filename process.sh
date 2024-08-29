e56bb06ad20489ea8ce371f63ea18c9d612e06ce
Shovon Bhowmick <shovon2464@gmail.com>
2024-08-29 16:06:43 -0600

    uvicorn command has been added

--- process.sh
+++ process.sh
@@ -1,1 +1,2 @@
-python manage.py runserver 192.168.1.111:8000
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/miniconda3/envs/privateGPTAPI/lib/python3.12/site-packages/nvidia/cudnn/lib
+uvicorn privateGPTAPI.asgi:application --host 192.168.1.111 --port 8000
